6f5ac247c90d5a1678c459ba20570e373815eadc
Evelyn Cheng <evelyncheng2003@gmail.com>
2024-07-07 15:04:31 -0700

    implementing backend

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
-import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
+import { DesktopDatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import Axios from "axios";
 
 class AddTodo extends Component {
   // Create a local react state of the this component with both content date property set to nothing.
@@ -25,7 +26,7 @@ class AddTodo extends Component {
 
   handleDateChange = (event) => {
     let date = null
-    if(event != null){
+    if (event != null) {
       date = new Date(event)
     }
     this.setState({
@@ -39,6 +40,22 @@ class AddTodo extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
     if (this.state.content.trim()) {
+      const jsonObject = {
+        id: this.state.id,
+        task: this.state.content,
+        currentDate: this.state.date,
+        dueDate: this.state.duedate
+      };
+      Axios({
+        method: "POST",
+        url: "http://localhost:8080/add/item",
+        data: { jsonObject },
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }).then(res => {
+        console.log(res.data.message);
+      });
       this.props.addTodo(this.state);
       this.setState({
         content: "",
@@ -63,13 +80,13 @@ class AddTodo extends Component {
           onChange={this.handleChange}
           value={this.state.content}
         />
-        <LocalizationProvider dateAdapter={AdapterDateFns}>         
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
           <DesktopDatePicker
-              id="new-item-date"
-              label="Due Date"
-              value={this.state.duedate}
-              onChange={this.handleDateChange}
-              renderInput={(params) => <TextField {...params} />}
+            id="new-item-date"
+            label="Due Date"
+            value={this.state.duedate}
+            onChange={this.handleDateChange}
+            renderInput={(params) => <TextField {...params} />}
           />
         </LocalizationProvider>
         <Button
